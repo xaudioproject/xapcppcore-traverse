ec14970a6aa80e8603b3078a6fefcb7735fdd200
Varme <hackerlinx@outlook.com>
2022-06-07 21:02:32 +0800

    Rolled library version to '1.0.0-alpha2'.
    Change(s):
      [1] Rolled library version from '1.0.0-alpha1' to '1.0.0-alpha2'.
    
    Contributor(s):
      [1] Varme <hackerlinx@outlook.com>

--- include/xap/core/json/version.h
+++ include/xap/core/json/version.h
@@ -11,7 +11,7 @@ namespace xap {
 namespace core {
 namespace json {
 
-#define XAP_CORE_JSON_VERSION "1.0.0-alpha1"
+#define XAP_CORE_JSON_VERSION "1.0.0-alpha2"
 
 }  //  namespace json
 }  //  namespace core
